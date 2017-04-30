2fac474366bbeb71f12b3612ecf50031726b391d
sasairc <sasairc@ssiserver.moe.hm>
2017-04-30 16:00:42 +0900

    modified:   test.bash

--- test.bash
+++ test.bash
@@ -21,8 +21,17 @@ function install_all() {
 function run_static_bin() {
 	echo "*** run ***"
 
+	SEED="ほげふがおうどんqwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+
 	wget https://raw.githubusercontent.com/sasairc/yasuna/master/quotes/yasuna-quotes
+
+	./sample/sample --help
+	./sample/sample --version
+
 	cat yasuna-quotes | ./sample/sample -e | ./sample/sample -d > dest.txt
+	diff -c yasuna-quotes dest.txt
+
+	cat yasuna-quotes | ./sample/sample -e "$SEED" | ./sample/sample -d -s "$SEED" > dest.txt
 	diff -c yasuna-quotes dest.txt
 
 	return $?
