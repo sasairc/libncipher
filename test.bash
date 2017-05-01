6a9b916f4a06befde25531dedf2dd1000038d1fa
sasairc <sasairc@ssiserver.moe.hm>
2017-05-01 15:22:59 +0900

    modified:   test.bash

--- test.bash
+++ test.bash
@@ -25,13 +25,13 @@ function run_static_bin() {
 
 	wget https://raw.githubusercontent.com/sasairc/yasuna/master/quotes/yasuna-quotes
 
-	./sample/sample --help
-	./sample/sample --version
+	./command/sample --help
+	./command/sample --version
 
-	cat yasuna-quotes | ./sample/sample -e | ./sample/sample -d > dest.txt
+	cat yasuna-quotes | ./command/sample -e | ./command/sample -d > dest.txt
 	diff -c yasuna-quotes dest.txt
 
-	cat yasuna-quotes | ./sample/sample -e -s "$SEED" | ./sample/sample -d -s "$SEED" > dest.txt
+	cat yasuna-quotes | ./command/sample -e -s "$SEED" | ./command/sample -d -s "$SEED" > dest.txt
 	diff -c yasuna-quotes dest.txt
 
 	return $?
